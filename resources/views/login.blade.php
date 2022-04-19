92ca01654760b58062e9c187186144263d022304
panpanYuya <yuya@sanbakaraisu.com>
2022-04-19 20:22:03 +0900

    ログイン画面のフロント作成

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,6 +5,7 @@
         <title>読書管理ツール</title>
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
+        <link rel="stylesheet" href="{{ asset('css/commonForm.css') }}" type="text/css">
     </head>
 
     <body class="antialiased">
@@ -12,26 +13,42 @@
         <div>
             <div class="main">
                 <div class="container">
-                    <div class="regist-form">
+                    <h1 class="title">ログイン</h1>
+                    <div class="common-form">
                         <form action="{{ url('/login/auth')}}" method="POST">
                             @csrf
-                            @error('user_name')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                            <label for="">
-                                ユーザー名
+                            <div class="user-info">
+                                @error('user_name')
+                                    <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
+                                <div class="tag">
+                                    <label for="user_name">
+                                        ユーザーネーム
+                                    </label>
+                                </div>
                                 <input type="text" id="user_name" name="user_name" minlength="8" maxlength="20" required value="{{ old('userName') }}">
-                            </label>
-                            @error('password')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                            <label for="">
-                                パスワード
+                            </div>
+                            <div class="user-info">
+                                @error('password')
+                                    <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
+                                <div class="tag">
+                                    <label for="password">
+                                        パスワード
+                                    </label>
+                                </div>
                                 <input type="password" id="password" name="password" minlength="8" maxlength="50" required >
-                            </label>
-                            <button type="submit">ログイン</button>
+                                <a href="#" class="forget-password">パスワードを忘れた方はこちら</a>
+                            </div>
+                            <div class="user-info">
+                                <button type="submit" class="common-button">ログイン</button>
+                            </div>
+                            <div class="user-info">
+                                <button type="submit" class="regist-button">新規登録</button>
+                            </div>
                         </form>
                     </div>
+                    <button type="submit" class="external_account-button">外部アカウントでログイン</button>
                 </div>
             </div>
         </div>
