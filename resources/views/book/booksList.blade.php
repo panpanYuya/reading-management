3cc193167b673db5f52fbec21deb8675aba0c530
panpanYuya <yuya@sanbakaraisu.com>
2022-04-25 20:34:17 +0900

    図書一覧画面のフロントデザイン作成

--- resources/views/book/booksList.blade.php
+++ resources/views/book/booksList.blade.php
@@ -23,6 +23,175 @@
                         </ul>
                     </div>
                     <div class="books-list">
+                        <div class="book">
+                            <div class="unread">
+                               <img src={{ asset('images/未達.png')}} alt="本の画像">
+                            </div>
+                            <div class="image"><img src={{ asset('images/sample1.jpg')}} alt="本の画像"></div>
+                            <div class="book_exaplain">
+                                <div class="title">本のタイトル</div>
+                                <div class="tags">
+                                    <p class="tags_title">見返しタグ</p>
+                                    <div class="tags_circle">
+                                        <div class="circle_first"></div>
+                                        <div class="circle_second"></div>
+                                        <div class="circle_third"></div>
+                                        <div class="circle_fource"></div>
+                                        <div class="circle_fifth"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="book">
+                            <div iv class="unread">
+                               <img src={{ asset('images/未達.png')}} alt="本の画像">
+                            </div>
+                            <div class="image"><img src={{ asset('images/sample2.jpg')}} alt="本の画像"></div>
+                            <div class="book_exaplain">
+                                <div class="title">本のタイトル</div>
+                                <div class="tags">
+                                    <p class="tags_title">見返しタグ</p>
+                                    <div class="tags_circle">
+                                        <div class="circle_first"></div>
+                                        <div class="circle_second"></div>
+                                        <div class="circle_third"></div>
+                                        <div class="circle_fource"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="book">
+                            <div iv class="unread">
+                               <img src={{ asset('images/未達.png')}} alt="本の画像">
+                            </div>
+                            <div class="image"><img src={{ asset('images/sample3.png')}} alt="本の画像"></div>
+                            <div class="book_exaplain">
+                                <div class="title">本のタイトル</div>
+                                <div class="tags">
+                                    <p class="tags_title">見返しタグ</p>
+                                    <div class="tags_circle">
+                                        <div class="circle_first"></div>
+                                        <div class="circle_second"></div>
+                                        <div class="circle_third"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="book">
+                            <div iv class="unread">
+                               <img src={{ asset('images/未達.png')}} alt="本の画像">
+                            </div>
+                            <div class="image"><img src={{ asset('images/sample4.jpg')}} alt="本の画像"></div>
+                            <div class="book_exaplain">
+                                <div class="title">本のタイトル</div>
+                                <div class="tags">
+                                    <p class="tags_title">見返しタグ</p>
+                                    <div class="tags_circle">
+                                        <div class="circle_first"></div>
+                                        <div class="circle_second"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="book">
+                            <div iv class="unread">
+                               <img src={{ asset('images/未達.png')}} alt="本の画像">
+                            </div>
+                            <div class="image"><img src={{ asset('images/sample5.png')}} alt="本の画像"></div>
+                            <div class="book_exaplain">
+                                <div class="title">本のタイトル</div>
+                                <div class="tags">
+                                    <p class="tags_title">見返しタグ</p>
+                                    <div class="tags_circle">
+                                        <div class="circle_first"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="book">
+                            <div iv class="unread">
+                               <img src={{ asset('images/未達.png')}} alt="本の画像">
+                            </div>
+                            <div class="image"><img src={{ asset('images/sample6.jpg')}} alt="本の画像"></div>
+                            <div class="book_exaplain">
+                                <div class="title">本のタイトル</div>
+                                <div class="tags">
+                                    <p class="tags_title">見返しタグ</p>
+                                    <div class="tags_circle">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="book">
+                            <div iv class="unread">
+                               <img src={{ asset('images/未達.png')}} alt="本の画像">
+                            </div>
+                            <div class="image"><img src={{ asset('images/sample6.jpg')}} alt="本の画像"></div>
+                            <div class="book_exaplain">
+                                <div class="title">本のタイトル</div>
+                                <div class="tags">
+                                    <p class="tags_title">見返しタグ</p>
+                                    <div class="tags_circle">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="book">
+                            <div iv class="unread">
+                               <img src={{ asset('images/未達.png')}} alt="本の画像">
+                            </div>
+                            <div class="image"><img src={{ asset('images/sample6.jpg')}} alt="本の画像"></div>
+                            <div class="book_exaplain">
+                                <div class="title">本のタイトル</div>
+                                <div class="tags">
+                                    <p class="tags_title">見返しタグ</p>
+                                    <div class="tags_circle">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="book">
+                            <div iv class="unread">
+                               <img src={{ asset('images/未達.png')}} alt="本の画像">
+                            </div>
+                            <div class="image"><img src={{ asset('images/sample6.jpg')}} alt="本の画像"></div>
+                            <div class="book_exaplain">
+                                <div class="title">本のタイトル</div>
+                                <div class="tags">
+                                    <p class="tags_title">見返しタグ</p>
+                                    <div class="tags_circle">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="book">
+                            <div iv class="unread">
+                               <img src={{ asset('images/未達.png')}} alt="本の画像">
+                            </div>
+                            <div class="image"><img src={{ asset('images/sample6.jpg')}} alt="本の画像"></div>
+                            <div class="book_exaplain">
+                                <div class="title">本のタイトル</div>
+                                <div class="tags">
+                                    <p class="tags_title">見返しタグ</p>
+                                    <div class="tags_circle">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="book">
+                            <div iv class="unread">
+                               <img src={{ asset('images/未達.png')}} alt="本の画像">
+                            </div>
+                            <div class="image"><img src={{ asset('images/sample6.jpg')}} alt="本の画像"></div>
+                            <div class="book_exaplain">
+                                <div class="title">本のタイトル</div>
+                                <div class="tags">
+                                    <p class="tags_title">見返しタグ</p>
+                                    <div class="tags_circle">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
