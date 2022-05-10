0319abdc14e5b36028bd6f7a584eeb1619724925
panpanYuya <yuya@sanbakaraisu.com>
2022-05-10 19:15:39 +0900

    本棚登録機能の機能仕様変更後のDBおよびmodelの修正

--- database/migrations/2022_02_19_060655_create_book_registrations_table.php
+++ database/migrations/2022_02_19_060655_create_book_registrations_table.php
@@ -18,14 +18,13 @@ class CreateBookRegistrationsTable extends Migration
             $table->collation = 'utf8mb4_bin';
             $table->increments('id');
             $table->string('api_id', 255)->comment('apiId');
+            $table->string('book_cover_url', 255)->nullable()->comment('表紙URL');
             $table->string('title', 255)->comment('タイトル');
             $table->string('author', 255)->comment('著者名');
-            $table->string('book_cover_url', 255)->nullable()->comment('表紙URL');
-            $table->unsignedInteger('genre_id')->nullable()->comment('ジャンルid');
+            $table->string('synopsis', 255)->comment('あらすじ');
             $table->dateTime('created_at', $precision = 0)->comment('作成時間');
             $table->dateTime('updated_at', $precision = 0)->comment('更新時間');
 
-            $table->foreign('genre_id')->references('id')->on('genre_masters');
         });
     }
 
